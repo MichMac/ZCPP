0003a43b47246233cad8dfd1a3bb3ba73e14117a
MichMac <mmaciejewski570@gmail.com>
2020-11-10 11:13:18 +0100

    wyjatek

--- Lista1/zadanie4.cpp
+++ Lista1/zadanie4.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <stdexcept>
+
 using namespace std;
 
 template <typename T, int N> class Wektor
@@ -23,25 +25,51 @@ public:
     T& operator [] (int n) { return wektor[n]; } // nadpisywanie
     T operator * (vector<int> const& wek2)
     {
-        for (int i = 0; i <= N - 1; i++)
+        try
         {
-            //cout << suma << endl;
-            suma += (wektor[i] * wek2[i]);
+            if (N != wek2.size())
+            {
+                throw runtime_error("Wektory nie maja tyle samo skladowych");
+            }
+            else
+            {
+                for (int i = 0; i <= N - 1; i++)
+                {
+                    //cout << suma << endl;
+                    suma += (wektor[i] * wek2[i]);
+                }
+                return suma;
+            }
         }
-        return suma;
+        catch (const exception& e)
+        {
+            cout << e.what() << endl;
+        }
+        return 0;
     }
 };
 
 int main()
 {
     cout << "Zadanie 4" << endl;
+
     Wektor <int, 3> wektor1;
     wektor1[0] = 2;
     wektor1[1] = 4;
-    wektor1[2] = 2;
+    wektor1[2] = 3;
+
     vector <int> wektor2;
     wektor2.push_back(4);
     wektor2.push_back(3);
     wektor2.push_back(5);
+
+    vector<int> wektor3;
+    wektor3.push_back(3);
+    wektor3.push_back(2);
+    wektor3.push_back(6);
+    wektor3.push_back(8);
+
     cout << wektor1 * wektor2 << endl;
+    cout << wektor1 * wektor3 << endl;
+    
 }
